f420410fd55f26a110c22568fcd5fa53a5b0fa6c
mwakitawalina <linamwakitawa@gmail.com>
2023-01-18 05:38:08 +0300

    dashboard updates

---
+++ lib/screens/map.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Map extends StatelessWidget {
+  const Map({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
