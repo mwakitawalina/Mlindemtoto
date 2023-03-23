8d645fb703cbf1b488470d77e8a9485935762210
mwakitawalina <linamwakitawa@gmail.com>
2023-03-23 19:52:35 +0300

    Latest commit on list devices

--- lib/screens/pairingdevice.dart
+++ lib/screens/pairingdevice.dart
@@ -22,7 +22,7 @@ class Pair extends StatelessWidget {
       
       body: Column(
         children: <Widget>[
-          Image(image: AssetImage("images/applewatch.png")),
+          Image(image: _getDeviceImage(singleDevice)),
           Padding(padding: EdgeInsets.all(20),
           child: Text("Huawei Bands will help connect you with your child for monitoring"),
           ),
@@ -40,5 +40,22 @@ class Pair extends StatelessWidget {
         
         ),
     );
+  }
+  _getDeviceImage(singleDevice){
+    switch(singleDevice){
+      case "Smart watch": {
+      return AssetImage("images/applewatch.png");
+    }
+    case "Smart Band" :{
+      return AssetImage("images/Huaweiband6.jpg");
+    }
+    case "Microchip" :{
+      return AssetImage("images/smartwatch.png");
+    }
+    case "HeadPhone" :{
+      return AssetImage("images/sumsungwatch.jpg");
+    }
+
+    }
   }
 }
